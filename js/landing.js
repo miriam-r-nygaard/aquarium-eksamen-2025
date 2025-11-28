6a457482d1ba149824e1b6de5796a4d2a9a4c555
miriam-r-nygaard <eaa25miny@students.eaaa.dk>
2025-11-28 10:53:03 +0100

    landingpage-css-index-js

--- js/landing.js
+++ js/landing.js
@@ -1,24 +1,21 @@
 "use strict";
 
-const nutte = document.getElementById('nutte')
-const vandmand = document.getElementById('vandmand')
+const nutte = document.getElementById("nutte");
+const vandmand = document.getElementById("vandmand");
 
-function swapImages() {
-  nutte.classList.toggle("nutte");
-  nutte.classList.toggle("swapped-right");
-  vandmand.classList.toggle("vandmand");
-  vandmand.classList.toggle("swapped-left");
-}
+let nutteState = "normal";
 
 if (nutte) {
   nutte.addEventListener("click", () => {
     const soundNutte = new Audio("./assets/audio/vilduspillemedmig.mp3");
-    //setTimeout() //swapImages ./assets/svg/nutte.svg
-    nutte.setAttribute("src","./assets/svg/nutte.svg")
-    setTimeout(() => nutte.setAttribute("src","./assets/svg/nutte-puff.svg"), 8000);
-    //
-    swapImages();
-    console.log('nutte')
+    //Toggle mellem nutte og nutte-puff
+    if (nutteState === "normal") {
+      nutte.setAttribute("src", "./assets/svg/nutte.svg");
+      nutteState = "puff";
+    } else {
+      nutte.setAttribute("src", "./assets/svg/nutte-puff.svg");
+    }
+    console.log("nutte virker", nutteState);
     soundNutte.play();
   });
 }
@@ -26,8 +23,7 @@ if (nutte) {
 if (vandmand) {
   const soundVandmand = new Audio("./assets/audio/sevideooglaer.mp3");
   vandmand.addEventListener("click", () => {
-    swapImages();
-    console.log('vandmand')
+    console.log("vandmand");
     soundVandmand.play();
   });
 }

3120e13bd9bd739a99813a0696eb5949762b3b49
miriam-r-nygaard <eaa25miny@students.eaaa.dk>
2025-11-28 11:57:24 +0100

    fixed-error-in-js-landing-nutte

--- js/landing.js
+++ js/landing.js
@@ -10,10 +10,11 @@ if (nutte) {
     const soundNutte = new Audio("./assets/audio/vilduspillemedmig.mp3");
     //Toggle mellem nutte og nutte-puff
     if (nutteState === "normal") {
-      nutte.setAttribute("src", "./assets/svg/nutte.svg");
+      nutte.setAttribute("src", "./assets/svg/nutte-puff.svg");
       nutteState = "puff";
     } else {
-      nutte.setAttribute("src", "./assets/svg/nutte-puff.svg");
+      nutte.setAttribute("src", "./assets/svg/nutte.svg");
+      nutteState = "normal";
     }
     console.log("nutte virker", nutteState);
     soundNutte.play();
